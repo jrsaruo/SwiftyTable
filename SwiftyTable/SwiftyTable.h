56163f1e2457ac407b930e58ff7a190a3208189b
Yusaku Nishi <jrsaruo@gmail.com>
2018-09-26 00:43:04 +0900

    first commit

---
+++ SwiftyTable/SwiftyTable.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyTable.h
+//  SwiftyTable
+//
+//  Created by Yusaku Nishi on 2018/09/25.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyTable.
+FOUNDATION_EXPORT double SwiftyTableVersionNumber;
+
+//! Project version string for SwiftyTable.
+FOUNDATION_EXPORT const unsigned char SwiftyTableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyTable/PublicHeader.h>
+
+
---
+++ SwiftyTableTests/SwiftyTableTests.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftyTableTests.swift
+//  SwiftyTableTests
+//
+//  Created by Yusaku Nishi on 2018/09/25.
+//
+
+import XCTest
+@testable import SwiftyTable
+
+class SwiftyTableTests: XCTestCase {
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+}
