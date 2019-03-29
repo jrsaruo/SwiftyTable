f44e3608fbc8e987503691664b791573e92589cf
Yusaku Nishi <jrsaruo@gmail.com>
2019-03-30 05:33:05 +0900

    add IndexPath initializers

--- SwiftyTable/Extensions/IndexPath+Extension.swift
+++ SwiftyTable/Extensions/IndexPath+Extension.swift
@@ -15,13 +15,28 @@ public extension IndexPath {
     }
     
     @inlinable
+    init<Item: CollectionItem, Section: CollectionSection>(item: Item, section: Section) {
+        self.init(item: item.rawValue, section: section.rawValue)
+    }
+    
+    @inlinable
     init<Section: TableSection>(row: Int, section: Section) {
         self.init(row: row, section: section.rawValue)
     }
     
     @inlinable
+    init<Section: CollectionSection>(item: Int, section: Section) {
+        self.init(item: item, section: section.rawValue)
+    }
+    
+    @inlinable
     init<Row: TableRow>(row: Row, section: Int) {
         self.init(row: row.rawValue, section: section)
+    }
+    
+    @inlinable
+    init<Item: CollectionItem>(item: Item, section: Int) {
+        self.init(item: item.rawValue, section: section)
     }
     
 }
